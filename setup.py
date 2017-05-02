2c1d808861deae5196ac910dd32abc8f53ba3579
Florian Mounier <florian.mounier@kozea.fr>
2017-05-02 15:57:10 +0200

    Let's release with make

--- setup.py
+++ setup.py
@@ -1,22 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# This file is part of unrest
-#
-# A troubling rest api library for sqlalchemy models
-# Copyright © 2017 Kozea Florian Mounier
-#
-# This library is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free
-# Software Foundation, either version 3 of the License, or (at your option) any
-# later version.
-#
-# This library is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
-# details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with pygal. If not, see <http://www.gnu.org/licenses/>.
 
 import os
 
